c6417e855371c1f3c8dc7ca7578c5167a429e9c5
nikita1298 <f201506100110090@gmail.com>
2020-10-11 21:33:05 +0530

    ListItem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ListItem from './ListItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list: [
+        "Item1",
+        "Item2",
+        "Item3",
+        "Item4",
+        "Item5",
+        "Item6",
+      ]
+    };
+  };
+
+
+  moveCard = (dragIndex, hoverIndex) => {
+    // const dragCard = cards[dragIndex];
+    console.log({ dragIndex, hoverIndex })
+    // setCards(update(cards, {
+    //   $splice: [
+    //     [dragIndex, 1],
+    //     [hoverIndex, 0, dragCard],
+    //   ],
+    // }));
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.list.map(listItem => (
+          <ListItem text={listItem} moveCard={this.moveCard} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App; 
