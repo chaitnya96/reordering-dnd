e45388649d2a450169f1112ff8bb3ec4d2bce591
Chaitnya <chaitnyaparikh@gmail.com>
2020-10-17 18:51:37 +0530

    fix(master): fixes on dnd mamanger

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
-import ListItem from './ListItem';
-
+import React, { Component } from "react";
+import ListItem from "./ListItem";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      list: [
-        "Item1",
-        "Item2",
-        "Item3",
-        "Item4",
-        "Item5",
-        "Item6",
-      ]
+      list: ["Item1", "Item2", "Item3", "Item4", "Item5", "Item6"],
     };
-  };
-
+  }
 
   moveCard = (dragIndex, hoverIndex) => {
     // const dragCard = cards[dragIndex];
-    console.log({ dragIndex, hoverIndex })
+    console.log({ dragIndex, hoverIndex });
     // setCards(update(cards, {
     //   $splice: [
     //     [dragIndex, 1],
@@ -32,12 +25,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.list.map(listItem => (
-          <ListItem text={listItem} moveCard={this.moveCard} />
-        ))}
+        <DndProvider backend={HTML5Backend}>
+          {this.state.list.map((listItem) => (
+            <ListItem text={listItem} moveCard={this.moveCard} />
+          ))}
+        </DndProvider>
       </div>
     );
   }
 }
 
-export default App; 
+export default App;
